6efda7f541fca5bf8ffbd5dd8288acc3681e9d13
HugoRoca <hugo.rock20@hotmail.com>
2018-11-05 17:12:32 -0500

    test

--- app.js
+++ app.js
@@ -2,17 +2,17 @@
     'use strict';
 
     const redis = require('redis'),
-        client = redis.createClient(6379, '127.0.0.1');
+        client = redis.createClient(6379, 'ecconsultorasqa.ombwyy.0001.use1.cache.amazonaws.com');
 
     //- Asignando valor en redis
     // const data = [{ "categorias": "", "codigoCampania": 201816, "codigoConsultora": 2826844, "codigoEstrategia": "" }, { "categorias": "", "codigoCampania": 201816, "codigoConsultora": 2826844, "codigoEstrategia": "" }, { "categorias": "", "codigoCampania": 201816, "codigoConsultora": 2826844, "codigoEstrategia": "" }, { "categorias": "", "codigoCampania": 201816, "codigoConsultora": 2826844, "codigoEstrategia": "" }, { "categorias": "", "codigoCampania": 201816, "codigoConsultora": 2826844, "codigoEstrategia": "" }];
-    // client.set('probandoAndo', JSON.stringify(data));
+    // client.set('probandoAndoRedis', JSON.stringify(data));
     // client.quit();
     //- Fin asignando valor en redis
 
 
     //- Obteniendo valor 
-    client.get('probandoAndo', (error, res) => {
+    client.get('probandoAndoRedis', (error, res) => {
         if (error) { throw error; }
         console.log(res);
         client.quit();
